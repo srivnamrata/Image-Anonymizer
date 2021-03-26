4e4e2933268c8b396f4d7f60808005a21b4d7032
Namrata <srivnamrata@gmail.com>
2021-03-26 23:29:51 +0530

    Update: LICENSE README.md censor_plate.py notebooks openalpr.conf runtime_data test_images

---
+++ notebooks/Users/namrata.srivastava@zf.com/Anonymize Images - ADAS/run.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %sh git clone https://github.com/srivnamrata/Image-Anonymizer.git --depth 1 --branch=master /dbfs/FileStore/Image-Anonymizer/
+
+# COMMAND ----------
+
+##%sh git -C /dbfs/FileStore/Image-Anonymizer/ pull
+
+# COMMAND ----------
+
+# MAGIC 
+# MAGIC %fs ls /dbfs/FileStore/
